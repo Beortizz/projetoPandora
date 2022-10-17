9ba594598f297952ed7eb3c0a6ece40624a8db41
Beortizz <beortizz@gmail.com>
2022-10-17 04:50:04 -0300

    css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <link href="/css/formStyle.css" rel="stylesheet">
+    {{-- <link href="/css/formStyle.css" rel="stylesheet"> --}}
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
